5d5d4368db0be7fc4acbe48a82ae7261c3e2d820
XoliKenny4 <xoli.ray435@gmail.com>
2023-11-17 03:44:50 -0800

    Good Good

--- simple_shell.c
+++ simple_shell.c
@@ -37,16 +37,6 @@ void execute_command(char *args[]) {
     }
 }
 
-void print_environment(void) {
-    extern char **environ;
-    char **env = environ;
-
-    while (*env != NULL) {
-        printf("%s\n", *env);
-        env++;
-    }
-}
-
 int main(void) {
     char input[MAX_INPUT_SIZE];
     char *args[MAX_ARGS];
@@ -66,8 +56,6 @@ int main(void) {
 
         if (strcmp(input, "exit") == 0) {
             break;
-        } else if (strcmp(input, "env") == 0) {
-            print_environment();
         } else {
             /* Tokenize input into arguments */
             token = strtok(input, " ");
@@ -79,47 +67,7 @@ int main(void) {
 
             args[i] = NULL; /* Set the last element to NULL */
 
-            if (access(args[0], X_OK) == 0) {
-                /* Execute the command directly if it is in the current directory */
-                execute_command(args);
-            } else {
-                /* Check the PATH environment variable */
-                char *path = getenv("PATH");
-                char *path_copy = strdup(path);
-                char *dir = strtok(path_copy, ":");
-
-                int command_found = 0;
-
-                while (dir != NULL) {
-                    /* Construct the full path to the executable */
-                    char *full_path = malloc(strlen(dir) + strlen(args[0]) + 2);
-                    if (full_path == NULL) {
-                        perror("malloc");
-                        exit(EXIT_FAILURE);
-                    }
-
-                    sprintf(full_path, "%s/%s", dir, args[0]);
-
-                    if (access(full_path, X_OK) == 0) {
-                        /* Execute the command */
-                        args[0] = full_path;
-                        execute_command(args);
-                        free(full_path);
-                        command_found = 1;
-                        break;
-                    }
-
-                    free(full_path);
-                    dir = strtok(NULL, ":");
-                }
-
-                free(path_copy);
-
-                if (!command_found) {
-                    /* Command not found */
-                    fprintf(stderr, "%s: 1: %s: not found\n", args[0], args[0]);
-                }
-            }
+            execute_command(args);
         }
     }
 
